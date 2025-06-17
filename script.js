bc238a8d3c4d7d5109903da92f164377317b15c4
Archana Nayak <archananayak212002@gmail.com>
2025-06-17 21:31:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+// JavaScript for progress bar
+const circles = document.querySelectorAll('.circle');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+let currentStep = 1;
+
+nextBtn.addEventListener('click', () => {
+  if (currentStep < circles.length) {
+    circles[currentStep].classList.add('active');
+    currentStep++;
+    prevBtn.disabled = false;
+    if (currentStep == circles.length) {
+      nextBtn.disabled = true;
+    }
+  }
+});
+
+prevBtn.addEventListener('click', () => {
+  if (currentStep > 1) {
+    circles[currentStep - 1].classList.remove('active');
+    currentStep--;
+    if (currentStep == 1) {
+      prevBtn.disabled = true;
+    }
+    nextBtn.disabled = false;
+  }
+});
+
